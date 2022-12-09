079276edbb3bc0e7f7a15f6528f2c268e0ebf578
sapling3120 <1794547296@qq.com>
2022-12-09 11:00:23 +0800

    v0.6

---
+++ R/add.R
@@ -0,0 +1,23 @@
+#' Title
+#'
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' a=myadd()
+#' b=myadd(4,5)
+
+#' c=myadd(y=1,x=2)
+#' myadd
+myadd <- function(x=1,y=2) {
+  res =x+y
+  return(res)
+
+}
+
+
+
